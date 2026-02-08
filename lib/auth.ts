337cc7f5dbf5c0bb774c898472f8b67f777838f4
Ooi Kim Siah <kimsiahooi@gmail.com>
2026-02-08 13:47:35 +0800

    feat: setup better auth

---
+++ lib/auth.ts
@@ -0,0 +1,23 @@
+import { compare, hash } from "bcrypt";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { admin } from "better-auth/plugins";
+import { prisma } from "@/lib/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "mysql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    password: {
+      hash: async (password) => {
+        return await hash(password, 12);
+      },
+      verify: async ({ hash, password }) => {
+        return await compare(password, hash);
+      },
+    },
+  },
+  plugins: [admin()],
+});
---
+++ prisma/seed.ts
@@ -0,0 +1,23 @@
+import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+
+async function main() {
+  await auth.api.createUser({
+    body: {
+      email: "admin@gmail.com", // required
+      password: "password", // required
+      name: "admin", // required
+      role: "admin",
+    },
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
