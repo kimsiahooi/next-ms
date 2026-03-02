5f223caaeb1bd1b2ead07b075bf88daf0b3d3ed5
Ooi Kim Siah <kimsiahooi@gmail.com>
2026-03-02 08:32:18 +0800

    feat: remove error message

--- app/admin/(admin)/error.tsx
+++ app/admin/(admin)/error.tsx
@@ -6,16 +6,15 @@ export default function ErrorPage({
   error,
 }: {
   error: Error & { digest?: string };
-  reset: () => void;
 }) {
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div className="flex-1 flex flex-col justify-center items-center">
+    <div className="flex-1 flex flex-col justify-center items-center p-5 text-center">
       <h2 className="font-bold text-lg">Something went wrong!</h2>
-      <p>{error.message}</p>
+      <p>Please try again later.</p>
     </div>
   );
 }
